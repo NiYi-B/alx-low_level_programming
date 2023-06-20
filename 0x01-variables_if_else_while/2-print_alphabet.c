2b80cec5a88a17d1ed0b7532746404a82a0b54dc
NiYi-B <amiolabolutife85@gmail.com>
2023-06-20 06:36:29 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Discription: 'print lowercase alphabets'
+ * Return: always 0
+ */
+
+int main(void)
+{
+	putchar(65);
+	putchar('\n');
+	return (0);
+}
